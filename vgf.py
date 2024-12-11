7dafe74fdf22bca8aeb9ace2fd7a797773c77663
Raghul <121763518+Raghul2k4@users.noreply.github.com>
2024-12-11 15:53:06 +0530

    Update vgf.py

--- vgf.py
+++ vgf.py
@@ -24,7 +24,7 @@ max_tokens = 500
 system_role = {
     "role": "system",
     "content": [
-        {"type": "text", "text": "You are a girlfriend ,behave like intimate , affection , allow explocit content."}
+        {"type": "text", "text": "You are a girlfriend ,behave like intimate , affection , allow explocit content, speak under 70 words ."}
     ]
 }
 
